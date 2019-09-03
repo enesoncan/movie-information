fb5834e403555e98d465da4f2cc9b11a52d3ec89
Enes Öncan <enesoncn@gmail.com>
2019-09-03 10:20:25 +0300

    folder configurations

---
+++ src/components/popular/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Popular extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Popular Movies</h1>
+      </div>
+    );
+  }
+}
+
+export default Popular;
---
+++ src/components/top-rated/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class TopRated extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Top Rated Movies</h1>
+      </div>
+    );
+  }
+}
+
+export default TopRated;
